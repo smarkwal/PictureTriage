b4d4fc72a993e9678148b44d2313139c49e15bd0
smarkwal <stephan@markwalder.net>
2026-04-05 13:41:15 +0200

    Feature: UI: Set application icons for window and macOS Dock

--- src/main/java/net/markwalder/picturetriage/PictureTriageApplication.java
+++ src/main/java/net/markwalder/picturetriage/PictureTriageApplication.java
@@ -1,6 +1,13 @@
 package net.markwalder.picturetriage;
 
+import java.awt.Taskbar;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 @SuppressWarnings("unused")
@@ -13,8 +20,44 @@ public class PictureTriageApplication extends Application {
     @Override
     @SuppressWarnings("unused")
     public void start(Stage primaryStage) {
+        // Set the macOS Dock icon (and other OS taskbar icons) programmatically.
+        // This overrides the generic Java/Gradle icon when running via `./gradlew run`.
+        setTaskbarIcon();
+
+        // Load and set application icons in multiple resolutions
+        primaryStage.getIcons().addAll(
+                new Image(getClass().getResourceAsStream("/icon_16.png")),
+                new Image(getClass().getResourceAsStream("/icon_32.png")),
+                new Image(getClass().getResourceAsStream("/icon_64.png")),
+                new Image(getClass().getResourceAsStream("/icon_128.png")),
+                new Image(getClass().getResourceAsStream("/icon_256.png")),
+                new Image(getClass().getResourceAsStream("/icon_512.png"))
+        );
+
         AppCoordinator coordinator = new AppCoordinator(primaryStage, getApplicationStylesheet());
         coordinator.begin();
+    }
+
+    /**
+     * Set the OS taskbar/Dock icon using the AWT Taskbar API.
+     * This is needed on macOS when the app is launched via Gradle, where the
+     * Dock would otherwise show the generic Java launcher icon.
+     */
+    private void setTaskbarIcon() {
+        if (!Taskbar.isTaskbarSupported()) {
+            return;
+        }
+        Taskbar taskbar = Taskbar.getTaskbar();
+        if (!taskbar.isSupported(Taskbar.Feature.ICON_IMAGE)) {
+            return;
+        }
+        try (InputStream is = getClass().getResourceAsStream("/icon_512.png")) {
+            if (is != null) {
+                taskbar.setIconImage(ImageIO.read(is));
+            }
+        } catch (IOException e) {
+            // Non-critical: ignore if the icon cannot be loaded
+        }
     }
 
     /**
